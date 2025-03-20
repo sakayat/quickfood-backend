d645aa6baa4d50d7cbc1815e916636ff79dd0356
sakayat <sakhyatkabir1@gmail.com>
2025-03-20 03:22:59 -0700

    updated register api

--- users/views.py
+++ users/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import User
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
 
 class RegisterView(APIView):
 
+    permission_classes = [permissions.AllowAny]
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            tokens = self.get_tokens_for_user(user)
             return Response(
-                {"user": UserSerializer(user).data}, status=status.HTTP_201_CREATED
+                {"user": UserSerializer(user).data, "tokens": tokens},
+                status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return {"refresh": str(refresh), "access": str(refresh.access_token)}
