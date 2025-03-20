3a7d2af35a92d0dfc2f862eee5b5290becf07c45
sakayat <sakhyatkabir1@gmail.com>
2025-03-20 03:39:01 -0700

    added login api

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import RegisterView
+from .views import RegisterView, LoginApiView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginApiView.as_view(), name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from .serializers import RegisterSerializer, UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -22,6 +23,36 @@ class RegisterView(APIView):
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return {"refresh": str(refresh), "access": str(refresh.access_token)}
+
+
+class LoginApiView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response(
+                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        tokens = self.get_tokens_for_user(user)
+
+        return Response({"user": UserSerializer(user).data, "tokens": tokens})
 
     def get_tokens_for_user(self, user):
         refresh = RefreshToken.for_user(user)
