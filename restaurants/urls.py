067136bbd30dc32ae48252034ce670c35c4d23c2
sakayat <sakhyatkabir1@gmail.com>
2025-03-20 11:13:36 -0700

    added updated restaurnt api

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import CreateRestaurantAPIView
+from .views import CreateRestaurantAPIView, UpdateRestaurantAPIView
 
 urlpatterns = [
     path(
         "create-restaurant/",
         CreateRestaurantAPIView.as_view(),
         name="create-restaurant",
+    ),
+    path(
+        "update-restaurant/",
+        UpdateRestaurantAPIView.as_view(),
+        name="update-restaurant",
     ),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from .serializers import RestaurantCreateSerializer
+from .models import Restaurant
 
 # Create your views here.
 
@@ -32,4 +33,37 @@ class CreateRestaurantAPIView(APIView):
                 "location": restaurant.location,
             }
             return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateRestaurantAPIView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        if self.request.user.role != "restaurant_owner":
+            return Response(
+                {"error": "Only restaurant owners can create restaurants"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        restaurant = Restaurant.objects.get(owner=self.request.user)
+        return restaurant
+
+    def put(self, request):
+        restaurant = self.get_object()
+        serializer = RestaurantCreateSerializer(
+            restaurant, data=request.data, partial=True
+        )
+
+        if serializer.is_valid():
+            restaurant = serializer.save()
+
+            response_data = {
+                "id": restaurant.id,
+                "owner": restaurant.owner.id,
+                "name": restaurant.name,
+                "description": restaurant.description,
+                "location": restaurant.location,
+            }
+            return Response(response_data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
